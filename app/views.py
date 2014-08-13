173e6fbbef4b067c0248cb76c10cee7b1c39f560
jarjun <arjun.jain@duke.edu>
2014-08-13 13:46:24 -0400

    init

--- app/views.py
+++ app/views.py
@@ -3,9 +3,12 @@ from app import app
 import requests
 
 @app.route('/')
+def default():
+	return render_template("rootpage.html")
+
 @app.route('/<name>')
 def index(name):
-	p = {"limit": 100}
+	p = {"limit": 50}
 	r = requests.get("https://api.twitch.tv/kraken/users/"+name+"/follows/channels", params = p)
 	r = r.json()
 	channellist = []
@@ -17,6 +20,22 @@ def index(name):
 			channellist.append((stream["stream"]["viewers"], streamname))
 
 	channellist = sorted(channellist)
-
 	fin = channellist[-1][1]
+	return render_template("stream.html", top = fin)
+
+@app.route('/<name>/<pos>')
+def indexpos(name,pos):
+	p = {"limit": 50}
+	r = requests.get("https://api.twitch.tv/kraken/users/"+name+"/follows/channels", params = p)
+	r = r.json()
+	channellist = []
+	for key in r["follows"]:
+		streamname = key["channel"]["name"]
+		stream = requests.get("https://api.twitch.tv/kraken/streams/" + streamname)
+		stream = stream.json()
+		if stream["stream"] != None:
+			channellist.append((stream["stream"]["viewers"], streamname))
+
+	channellist = sorted(channellist)
+	fin = channellist[-int(pos)][1]
 	return render_template("stream.html", top = fin)
